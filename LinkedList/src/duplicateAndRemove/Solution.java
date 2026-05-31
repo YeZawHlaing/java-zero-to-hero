85a62367ef929f2af03998648cac80e69206f99f
yezawhlaing <yezawhlaing0@gmail.com>
2026-05-31 13:25:19 +0630

    linkednode

---
+++ LinkedList/src/duplicateAndRemove/Solution.java
@@ -0,0 +1,62 @@
+package duplicateAndRemove;
+
+
+import java.util.HashSet;
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+      ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+
+
+class Solution {
+    public static ListNode deleteDuplicates(ListNode head) {
+
+        HashSet<Integer> seen= new HashSet<>();
+        ListNode current=head;
+
+        seen.add(current.val);
+
+        while (current != null && current.next != null){
+            if (seen.contains(current.next.val)){
+                current.next = current.next.next;
+            }
+            else {
+                seen.add(current.next.val);
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    private static ListNode createLinkedList(int[] arr) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for (int num : arr) {
+            curr.next = new ListNode(num);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String args[]){
+        ListNode head= new ListNode();
+        head=createLinkedList(new int[]{1,2,1,3,1});
+
+        ListNode ans=deleteDuplicates(head);
+
+        System.out.println("Deleted LinkedNode: ");
+        System.out.println(ans.val);
+
+
+    }
+}
+
+
+
+
